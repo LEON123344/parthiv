33c670fe4cb35f81d5a958393f5241ea2dd94247
LEON123344 <153254746+LEON123344@users.noreply.github.com>
2023-12-08 17:57:45 +0530

    Update parthiv.py

--- parthiv.py
+++ parthiv.py
@@ -1,17 +1,23 @@
-def maxArea(height: List[int]) -> int:
-    # This variable will store the maximum area
-    max_area = -maxsize
-    # Left and right pointers
-    left = 0
-    right = len(height) - 1
-    # Loop until the two pointers meet
-    while left < right:
-        # Shorter of the two lines
-        shorter_line = min(height[left], height[right])
-        max_area = max(max_area, shorter_line * (right - left))
-        # If there is a longer vertical line present
-        if height[left] < height[right]:
-            left += 1
-        else:
-            right -= 1
-    return max_area
+def max_area(height):
+  max_water = 0
+  left_pointer, right_pointer = 0 , len(height) - 1
+
+  while left_pointer < right_pointer:
+    height_left , height_right = height[left_pointer], height[right_pointer]
+    width = right_pointer - left_pointer
+    current_water = min(height_left, height_right) * width
+    max-water = max(max_water, current_water)
+
+    if height_left < height_right:
+      left_pointer += 1
+    else:
+      right_pointer -= 1
+
+  return f"Input: height = {height}\nOutput: {max_water}"
+ 
+#maincode
+#we define the heights as a list 
+#max_area function is used which returns maximum area
+height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+result = max_area(height)
+print(result)
